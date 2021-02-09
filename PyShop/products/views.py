8c7c27285c68887725b1af155dcd31373cc017e2
Engr-Asif <asif.engr.official@gmail.com>
2021-02-09 10:42:53 +0500

    Complete Code

---
+++ PyShop/products/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Product,Offer
+
+
+def index (request):
+    products = Product.objects.all()
+    return render(request,'index.html',{'products':products})
